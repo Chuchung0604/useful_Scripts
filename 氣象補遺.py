39687bfb199edba558d0b8e364180b8dde7f8546
Chuchung Chen <33993438+Chuchung0604@users.noreply.github.com>
2025-10-28 08:54:25 +0800

    upload

---
+++ 氣象補遺.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 14:57:02 2025
+
+@author: ccchen
+"""
+import pandas as pd
+import csv
+
+def readWea( weatype, year, lon,lat, doy):
+    if year %4 == 0:
+        yearNo = 366
+    else:
+        yearNo = 365
+       
+    value = []    
+    # path of first year
+    filename = "G:/SynologyDrive/weather/網格化觀測日資料_5km/觀測_日資料_臺灣_%s/觀測_日資料_臺灣_%s_%d.csv" %(
+        weatype, weatype, year)
+    
+
+    # read weather of first year
+    with open(filename, newline='') as csvfile:
+        df = csv.reader(csvfile, delimiter = ',')
+        next(df) # skip header
+        for row in df:
+            if float(row[0]) == lon and float(row[1]) == lat:  
+                value =row[doy+2-1]
+                # +2 是lon 和lat
+                # -1 是 考慮python從0起算，DOY從1起算
+                # for d in range(yearNo + 2):
+                #     if d < 2: 
+                #         continue
+                #     value.append(float(row[d]))
+                #            #print(row[d])
+
+    return value
+
+
+
+
+
+
+
+wea = pd.read_csv("TARIweather.csv", skiprows=1, encoding="big5")
+
+
+for index, row in wea.iterrows():
+    year = row['Year']
+    DOY = row['DOY']
+    
+    if pd.isnull(row["Tavg"]):         
+        print(year, DOY, "Tavg")
+        temp = readWea(weatype="平均溫", year=year, lon=120.7, lat=24.0, doy = DOY)
+        wea.loc[index, 'Tavg']  = temp
+
+
+    if pd.isnull(row["Tmax"]):         
+        print(year, DOY, "Tmax")
+        temp = readWea(weatype="最高溫", year=year, lon=120.7, lat=24.0, doy= DOY)
+        wea.loc[index, 'Tmax']  = temp
+    
+    if pd.isnull(row["Tmin"]):         
+        print(year, DOY)
+        temp = readWea(weatype="最低溫", year=year, lon=120.7, lat=24.0, doy=DOY)
+        wea.loc[index, 'Tmin']  = temp
+
+    
+
+wea.to_csv("TARIwea_fill.csv", header=True)    
+
